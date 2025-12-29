e7e6a2b797fd2c2dcd1a19b96c105540c3f45366
Aissa <aissa.allay@gmail.com>
2025-12-29 13:01:19 +0100

    Inicio del proyecto

---
+++ api-libros-usuarios/api-libros-usuarios/src/main/java/com/api/api_libros_usuarios/ApiLibrosUsuariosApplication.java
@@ -0,0 +1,13 @@
+package com.api.api_libros_usuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLibrosUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiLibrosUsuariosApplication.class, args);
+	}
+
+}
